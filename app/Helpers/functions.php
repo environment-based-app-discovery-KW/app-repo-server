0be7ea17bb48dc1d0fdd799c0bfa03ab8d1fa174
Ke Wang <453587854@qq.com>
2018-04-13 23:20:43 +0800

    add helper functions

---
+++ app/Helpers/functions.php
@@ -0,0 +1,37 @@
+<?php
+function join_paths() {
+    $paths = array();
+
+    foreach (func_get_args() as $arg) {
+        if ($arg !== '') { $paths[] = $arg; }
+    }
+
+    return preg_replace('#/+#','/',join('/', $paths));
+}
+
+function put_file($file_path)
+{
+    $hash = hash_file('sha256', $file_path);
+    copy($file_path, storage_path(join_paths('files_bucket', $hash)));
+    return $hash;
+}
+
+function rrmdir($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir."/".$object))
+                    rrmdir($dir."/".$object);
+                else
+                    unlink($dir."/".$object);
+            }
+        }
+        rmdir($dir);
+    }
+}
+
+function error($msg, $code = 500)
+{
+    return Response::json(['errMsg' => $msg], $code);
+}
